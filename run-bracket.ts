16f573fd77297fa2dc2206828749cea93fb48b5a
oridim <125223080+oridim@users.noreply.github.com>
2025-01-07 07:48:36 -0500

    feat: updated bracket script to actually run a bracket

--- run-bracket.ts
+++ run-bracket.ts
@@ -1,8 +1,9 @@
+import { makeBracket } from './bracket.ts';
 import {
     cloneCompetitorRepositories,
     transformCompetitorData,
 } from './competitor.ts';
-import { exec, generateDaySeed } from './util.ts';
+import { generateDaySeed } from './util.ts';
 
 import COMPETITOR_MANIFEST from './competitors.json' with { type: 'json' };
 
@@ -11,3 +12,14 @@ const COMPETITORS = transformCompetitorData(COMPETITOR_MANIFEST);
 const SEED = generateDaySeed();
 
 await cloneCompetitorRepositories(COMPETITORS);
+
+const bracket = makeBracket({
+    competitors: COMPETITORS,
+    matchesBestOf: 5,
+    seed: SEED,
+    suddenDeathMax: 3,
+});
+
+const results = await bracket.computeBracket();
+
+console.log({ results });
