cebaf595366409307fd1c4265508e29215ef8ab2
oridim <125223080+oridim@users.noreply.github.com>
2025-01-07 07:19:44 -0500

    feat: added initial game engine cli wrapper function

--- competitor.ts
+++ competitor.ts
@@ -1,8 +1,12 @@
 import { join } from '@std/path';
 
+import { exec } from './util.ts';
+
 const DIRECTORY_COMPETITOR_REPOSITORIES = './competitors';
 
 const FILE_COMPETITOR_PLAYER = './mod.js';
+
+const FILE_GAME_ENGINE = './.bin/dotsandboxes-linux';
 
 export interface ICompetitorData {
     readonly name: string;
@@ -11,11 +15,21 @@ export interface ICompetitorData {
 }
 
 export interface ICompetitor {
-    readonly filePath: string;
-
     readonly name: string;
 
+    readonly playerFile: string;
+
     readonly repository: URL;
+}
+
+export interface ISimulateCompetitorsOptions {
+    readonly gridColumns?: number;
+
+    readonly gridRows?: number;
+
+    readonly seed?: number;
+
+    readonly timeout?: number;
 }
 
 export async function cloneCompetitorRepositories(
@@ -33,6 +47,55 @@ export async function cloneCompetitorRepositories(
     );
 }
 
+export async function simulateCompetitors(
+    options: ISimulateCompetitorsOptions,
+    ...competitors: ICompetitor[]
+): Promise<ICompetitor | null> {
+    const { gridColumns, gridRows, seed, timeout } = options;
+
+    const cliOptions: string[] = [];
+
+    if (gridColumns !== undefined) {
+        cliOptions.push(
+            '--grid-columns',
+            gridColumns.toString(),
+        );
+    }
+
+    if (gridRows !== undefined) {
+        cliOptions.push(
+            '--grid-rows',
+            gridRows.toString(),
+        );
+    }
+
+    if (seed !== undefined) {
+        cliOptions.push(
+            '--seed',
+            seed.toString(),
+        );
+    }
+
+    if (timeout !== undefined) {
+        cliOptions.push(
+            '--timeout',
+            timeout.toString(),
+        );
+    }
+
+    const playerFiles = competitors
+        .map((competitors) => {
+            const { playerFile } = competitors;
+
+            return playerFile;
+        });
+
+    // **TODO:** game log parsing
+    // exec(FILE_GAME_ENGINE, 'simulate', ...cliOptions, ...playerFiles);
+
+    return null;
+}
+
 export function transformCompetitorData(
     competitorManifest: ICompetitorData[],
 ): ICompetitor[] {
@@ -42,15 +105,15 @@ export function transformCompetitorData(
 
             const repositoryURL = new URL(repository);
 
-            const filePath = join(
+            const playerFile = join(
                 DIRECTORY_COMPETITOR_REPOSITORIES,
                 name,
                 FILE_COMPETITOR_PLAYER,
             );
 
             return {
-                filePath,
                 name,
+                playerFile,
                 repository: repositoryURL,
             };
         });
