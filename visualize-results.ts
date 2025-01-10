1c6d4b012dee8c0af31a4169956fb4c5de10d0f9
oridim <125223080+oridim@users.noreply.github.com>
2025-01-10 13:40:19 -0500

    feat: prettify the HTML output

--- visualize-results.ts
+++ visualize-results.ts
@@ -1,15 +1,15 @@
-import { renderToString } from 'preact-render-to-string';
+import { render } from 'preact-render-to-string/jsx';
 
 import { Document } from './lib/html.tsx';
 import type { IRunResults } from './lib/results.ts';
 import { FILE_RUN_LOG } from './lib/run.ts';
 
-const HEADER_DOCTYPE = '<!DOCTYPE html>';
+const HEADER_DOCTYPE = '<!DOCTYPE html>\n';
 
 const jsonPayload = await Deno.readTextFile(FILE_RUN_LOG);
 const runResults = JSON.parse(jsonPayload) as IRunResults;
 
 const document = Document();
-const rendered = renderToString(document);
+const rendered = render(document, {}, { pretty: true });
 
 console.log(HEADER_DOCTYPE + rendered);
