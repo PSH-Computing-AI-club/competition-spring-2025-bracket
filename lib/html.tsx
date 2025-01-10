267674306f00a956b7e3105a52209f39a606edb3
oridim <125223080+oridim@users.noreply.github.com>
2025-01-10 13:47:26 -0500

    fixed: whoops, forgot the head tag

--- lib/html.tsx
+++ lib/html.tsx
@@ -1,16 +1,18 @@
 export function Document() {
     return (
         <html lang='en'>
-            <meta charset='UTF-8' />
-            <meta
-                name='viewport'
-                content='width=device-width, initial-scale=1.0'
-            />
+            <head>
+                <meta charset='UTF-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1.0'
+                />
 
-            <title>
-                Bracket :: Spring '25 Blossoming Battlegrounds — PSH Computing &
-                AI Club
-            </title>
+                <title>
+                    Bracket :: Spring '25 Blossoming Battlegrounds — PSH
+                    Computing & AI Club
+                </title>
+            </head>
 
             <body>
                 hello world
