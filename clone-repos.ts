bf35ca0f029253c280ac1cdd4a57c6dc9a1d9829
oridim <125223080+oridim@users.noreply.github.com>
2025-01-10 11:30:24 -0500

    feat: clone competitor repos by their identifiers

--- clone-repos.ts
+++ clone-repos.ts
@@ -6,8 +6,6 @@ import { exec } from './lib/util.ts';
 
 import COMPETITOR_MANIFEST from './competitors.json' with { type: 'json' };
 
-// **TODO:** make `./competitors` directory, clone into that directory
-
 await Deno.mkdir(DIRECTORY_COMPETITOR_REPOSITORIES, { recursive: true });
 
 const COMPETITORS = await transformCompetitorData(COMPETITOR_MANIFEST);
@@ -15,9 +13,14 @@ const COMPETITORS = await transformCompetitorData(COMPETITOR_MANIFEST);
 await Promise.all(
     COMPETITORS
         .map(async (competitor) => {
-            const { repository } = competitor;
+            const { repository, repositoryDirectory } = competitor;
 
-            const output = await exec('git', 'clone', repository.toString());
+            const output = await exec(
+                'git',
+                'clone',
+                repository.toString(),
+                repositoryDirectory,
+            );
 
             console.log(output);
         }),
