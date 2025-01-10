1ec949a9978d3ac9b6a525aa203132e8d4aa563e
oridim <125223080+oridim@users.noreply.github.com>
2025-01-10 11:27:50 -0500

    feat: make repository directory if not found

--- clone-repos.ts
+++ clone-repos.ts
@@ -1,8 +1,11 @@
+import { DIRECTORY_COMPETITOR_REPOSITORIES } from './lib/competitor.ts';
 import { exec } from './lib/util.ts';
 
 import COMPETITOR_MANIFEST from './competitors.json' with { type: 'json' };
 
 // **TODO:** make `./competitors` directory, clone into that directory
+
+await Deno.mkdir(DIRECTORY_COMPETITOR_REPOSITORIES, { recursive: true });
 
 await Promise.all(
     COMPETITOR_MANIFEST
