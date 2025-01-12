567e2a3e57e64848bee31372e546df18ee942d8c
oridim <125223080+oridim@users.noreply.github.com>
2025-01-11 20:02:40 -0500

    fix: forgot to make the size relative when I grabbed the SVG

--- lib/icons.tsx
+++ lib/icons.tsx
@@ -49,8 +49,8 @@ export function Trophy(props: IIconProps = {}) {
         <svg
             xmlns='http://www.w3.org/2000/svg'
             class={`lucide lucide-medal ${className}`}
-            width='24'
-            height='24'
+            width='1.25em'
+            height='1.25em'
             viewBox='0 0 24 24'
             fill='none'
             stroke='currentColor'
