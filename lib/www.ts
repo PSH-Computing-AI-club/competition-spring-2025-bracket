b60338c065b0318c97af7b02249e8b634fe248a0
oridim <125223080+oridim@users.noreply.github.com>
2025-01-13 05:41:10 -0500

    feat: added creation of log directories

--- lib/www.ts
+++ lib/www.ts
@@ -1,5 +1,13 @@
 import { join } from '@std/path';
 
-export const DIRECTORY_WWW_OUTPUT = `./dist/www`;
+export const DIRECTORY_WWW_OUTPUT = join(`./dist/www`);
+
+export const DIRECTORY_LOG_OUTPUT = join(DIRECTORY_WWW_OUTPUT, 'logs');
+
+export const DIRECTORY_BRACKET_LOGS = join(DIRECTORY_LOG_OUTPUT, 'brackets');
+
+export const DIRECTORY_MATCH_LOGS = join(DIRECTORY_LOG_OUTPUT, 'matches');
 
 export const FILE_WWW_LANDING_INDEX = join(DIRECTORY_WWW_OUTPUT, 'index.html');
+
+export const FILE_LOG_LANDING_INDEX = join(DIRECTORY_LOG_OUTPUT, 'index.html');
--- visualize-results.ts
+++ visualize-results.ts
@@ -3,9 +3,21 @@ import { render } from 'preact-render-to-string/jsx';
 import { BracketView, HEADER_DOCTYPE } from './lib/html.tsx';
 import type { IRunResults } from './lib/results.ts';
 import { FILE_RUN_LOG } from './lib/run.ts';
-import { DIRECTORY_WWW_OUTPUT, FILE_WWW_LANDING_INDEX } from './lib/www.ts';
+import {
+    DIRECTORY_BRACKET_LOGS,
+    DIRECTORY_MATCH_LOGS,
+    DIRECTORY_WWW_OUTPUT,
+    FILE_WWW_LANDING_INDEX,
+} from './lib/www.ts';
 
-await Deno.mkdir(DIRECTORY_WWW_OUTPUT, { recursive: true });
+await Promise.all([
+    DIRECTORY_WWW_OUTPUT,
+    DIRECTORY_BRACKET_LOGS,
+    DIRECTORY_MATCH_LOGS,
+]
+    .map(
+        (directory) => Deno.mkdir(directory, { recursive: true }),
+    ));
 
 const jsonPayload = await Deno.readTextFile(FILE_RUN_LOG);
 const runResults = JSON.parse(jsonPayload) as IRunResults;
