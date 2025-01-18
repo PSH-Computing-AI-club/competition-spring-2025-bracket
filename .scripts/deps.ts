bea8995c1708a4b999caab53d7c8955db089fe37
oridim <125223080+oridim@users.noreply.github.com>
2025-01-18 17:43:35 -0500

    fix: added missing deps to cache script

--- .scripts/deps.ts
+++ .scripts/deps.ts
@@ -1,6 +1,10 @@
 // We want a script file to collect all of the depenedencies we are using
 // so that Deno can easily cache them ahead-of-time rather than just-in-time.
 
-import 'slug';
+import '@octokit/app';
+import '@octokit/auth-app';
 import '@std/path';
 import '@std/random';
+import 'preact';
+import 'preact-render-to-string';
+import 'slug';
